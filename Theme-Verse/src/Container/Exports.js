8806b2fc58c218051d7b9f49510cb5946c7db617
Gitstar-OC <chandankarom07@gmail.com>
2024-05-05 15:25:48 +0530

    Started Tailwind and working no Navbar, fixed some error, app is now working

--- Theme-Verse/src/Container/Exports.js
+++ Theme-Verse/src/Container/Exports.js
@@ -1,1 +1,1 @@
-export {Navigation} from './Navigation';
+export { default as Navigation } from './Navigation';
--- Theme-Verse/src/Container/Navigation.jsx
+++ Theme-Verse/src/Container/Navigation.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Navigation = () => {
   return (
     <>
-
+    <header>
+    </header>
+    <nav className="font-bold font-mono text-4xl bg-black text-white"> 
+      This is navbar
+    </nav>
     </>
   )
 }
