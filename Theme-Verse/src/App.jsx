b86dcc3fb02fe999d389fd87639868f8bebb32fb
Gitstar-OC <chandankarom07@gmail.com>
2024-05-05 15:25:48 +0530

    Started Tailwind and working no Navbar, fixed some error, app is now working

--- Theme-Verse/src/App.jsx
+++ Theme-Verse/src/App.jsx
@@ -1,10 +1,13 @@
 import { Navigation } from './Container/Exports'
-import './App.css'
+import './index.css';
 
 function App() {
 
   return (
     <>
+    <h1 className="text-3x1 font-bold underline text-center">
+    Hello World
+    </h1>
     <Navigation />
     </>
   )
