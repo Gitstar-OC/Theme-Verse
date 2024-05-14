67e84c589a3035182b5ce965ed8d583c01099a50
Gitstar-OC <chandankarom07@gmail.com>
2024-05-14 16:53:21 +0530

    Added cart

--- Theme-Verse/src/Container/Navigation.jsx
+++ Theme-Verse/src/Container/Navigation.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import TV1 from "../assets/Logos/TV.1.png"
+import { FaOpencart } from "react-icons/fa";
 
 const Navigation = () => {
   return (
     <>
     <header className='bg-slate-400 flex'>
     <img className="w-[160px] h-[120px]" src={TV1} alt="logo" />
+    <FaOpencart />
     <nav className="font-bold font-mono text-4xl bg-black text-white flex"> 
 
     {/* Soon adding other navigation paths and completing the figma filew */}
