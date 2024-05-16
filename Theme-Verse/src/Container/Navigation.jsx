8910690046aca34407352495d661af9ac26642e4
Gitstar-OC <chandankarom07@gmail.com>
2024-05-16 17:13:12 +0530

    Added background image for navigation bar, in both light and dark theme

--- Theme-Verse/src/Container/Navigation.jsx
+++ Theme-Verse/src/Container/Navigation.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import TV1 from "../assets/Logos/TV.1.png"
 import { FaOpencart } from "react-icons/fa";
 
 const Navigation = () => {
   return (
     <>
-    <header className='bg-slate-400 flex'>
-    <img className="w-[160px] h-[120px]" src={TV1} alt="logo" />
-    <FaOpencart />
+    <header className='bg-slate-400 flex bg-gradient-to-b from-blue-200 via-blue-700 to-blue-200'>
+        <FaOpencart />
     <nav className="font-bold font-mono text-4xl bg-black text-white flex"> 
 
     {/* Soon adding other navigation paths and completing the figma filew */}
