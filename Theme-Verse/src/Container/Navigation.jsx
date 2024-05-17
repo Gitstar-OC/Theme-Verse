4ac995c11e2d11754da801cf405e72d85e65e5f2
Gitstar-OC <chandankarom07@gmail.com>
2024-05-17 22:59:28 +0530

    Completed total navbar for large screen, working on small sized now

--- Theme-Verse/src/Container/Navigation.jsx
+++ Theme-Verse/src/Container/Navigation.jsx
@@ -1,6 +1,6 @@
-import { FaOpencart, FaSun, FaMoon } from "react-icons/fa";
+import { FaOpencart, FaSun, FaMoon } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import Logo from "../assets/Theme-Verse.svg"
+import Logo from "../assets/Theme-Verse.svg";
 import { useState, useEffect } from 'react';
 
 const Navigation = () => {
@@ -19,36 +19,35 @@ const Navigation = () => {
   };
 
   return (
-    <>
-      <header className='bg-hero w-full h-[55px] bg-cover flex items-center px-4' id="section">
-        <div className='flex justify-between items-center w-full max-w-screen-xl mx-auto'>
-          <div className='flex items-center space-x-8'>
-            <img src={Logo} alt="Logo" className='h-8' />
-          </div>
-          <nav className='flex space-x-8'>
-            <Link to="/" className='navItem theme-text'>Home</Link>
-            <Link to="/themes" className='navItem theme-text'>Themes</Link>
-            <Link to="/about" className='navItem theme-text'>About</Link>
-            <Link to="/contact" className='navItem theme-text'>Contact</Link>
-            <Link to="/cart" className='navItem theme-text'>
-              <div className='flex items-center space-x-1'>
-                <FaOpencart />
-                <span>Cart</span>
-              </div>
-            </Link>
-            <button onClick={toggleTheme} className='navItem theme-text'>
-              <div className={`toggle-circle ${theme}`}>
-                {theme === 'light' ? <FaSun /> : <FaMoon />}
-              </div>
-            </button>
-          </nav>
+    <header className='bg-hero w-full h-[55px] bg-cover flex items-center px-4' id="section">
+      <div className='flex justify-between items-center w-full max-w-screen-xl mx-auto'>
+        <div className='flex items-center space-x-8'>
+          <img src={Logo} alt="Logo" className='h-8' />
         </div>
-      </header>
-    </>
+        <nav className='flex space-x-8'>
+          <Link to="/" className='navItem theme-text hover:text-[#09FFB5]'>Home</Link>
+          <Link to="/themes" className='navItem theme-text hover:text-[#09FFB5]'>Themes</Link>
+          <Link to="/about" className='navItem theme-text hover:text-[#09FFB5] '>About</Link>
+          <Link to="/contact" className='navItem theme-text hover:text-[#09FFB5]'>Contact</Link>
+          <Link to="/cart" className='navItem theme-text hover:text-[#09FFB5]'>
+            <div className='flex items-center space-x-1 hover:text-[#09FFB5]'>
+              <FaOpencart />
+              <span>Cart</span>
+            </div>
+          </Link>
+          <button onClick={toggleTheme} className='navItem theme-text'>
+            <div className={`toggle-circle ${theme}`}>
+              {theme === 'light' ? <FaSun /> : <FaMoon />}
+            </div>
+          </button>
+        </nav>
+      </div>
+    </header>
   );
 }
 
 export default Navigation;
+
 
 
 /*        <div className="flex flex-wrap gap-10 mb-10">
