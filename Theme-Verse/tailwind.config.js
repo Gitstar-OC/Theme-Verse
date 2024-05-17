38ae539171f767cf7a9f257083b32eb3deda1edf
Gitstar-OC <chandankarom07@gmail.com>
2024-05-17 21:33:31 +0530

    Updated hover properties

--- Theme-Verse/tailwind.config.js
+++ Theme-Verse/tailwind.config.js
@@ -1,3 +1,44 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./src/**/*.{html,js,jsx}"],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         'hero': "url('./assets/NavbarBackground.png')",
+//       },
+//       fontFamily: {
+//         'cF': ['CF', 'sans-serif'],
+//       },
+//       colors: {
+//         'primary': '#ffffff',
+//         'secondary': '#ffcc00',
+//       },
+//     },
+//   },
+//   plugins: [
+//     function ({ addUtilities }) {
+//       const newUtilities = {
+//         '.navItem': {
+//           '@apply text-primary font-cF text-[30px] font-normal leading-normal relative': {},
+//           '&:hover': {
+//             '@apply text-secondary underline': {},
+//           },
+//           '&:active': {
+//             '@apply text-primary opacity-75': {},
+//           },
+//           '&::after': {
+//             content: '""',
+//             '@apply block h-1 bg-secondary absolute bottom-0 left-0 transform scale-x-0 transition-transform duration-300': {},
+//           },
+//           '&:hover::after': {
+//             '@apply scale-x-100': {},
+//           },
+//         },
+//       }
+//       addUtilities(newUtilities)
+//     }
+//   ],
+// }
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
@@ -9,26 +50,30 @@ export default {
       fontFamily: {
         'cF': ['CF', 'sans-serif'],
       },
-      // Custom utility class
-      textColor: {
-        'white': '#ffffff',
+      colors: {
+        'primary': '#ffffff',
+        'secondary': '#09FFB5',
       },
-      fontSize: {
-        '30px': '30px',
-      },
-      leading: {
-        'normal': 'normal',
-      },
-      fontWeight: {
-        'normal': '400',
-      }
     },
   },
   plugins: [
     function ({ addUtilities }) {
       const newUtilities = {
         '.navItem': {
-          '@apply text-white font-cF text-30px font-normal leading-normal': {},
+          '@apply text-primary font-cF text-[30px] font-normal leading-normal relative': {},
+          '&:hover': {
+            '@apply text-secondary underline': {},
+          },
+          '&:active': {
+            '@apply text-primary opacity-75': {},
+          },
+          '&::after': {
+            content: '""',
+            '@apply block h-1 bg-secondary absolute bottom-0 left-0 transform scale-x-0 transition-transform duration-300': {},
+          },
+          '&:hover::after': {
+            '@apply scale-x-100': {},
+          },
         },
       }
       addUtilities(newUtilities)
