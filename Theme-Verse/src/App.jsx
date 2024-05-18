69b4aba7b68b95e26f33537c43eaaca4f2fba36e
Gitstar-OC <chandankarom07@gmail.com>
2024-05-18 12:53:39 +0530

    Updated app.jsx file, app is now working, completing footer and main file

--- Theme-Verse/src/App.jsx
+++ Theme-Verse/src/App.jsx
@@ -1,10 +1,12 @@
 // import { BrowserRouter as Route, Routes } from 'react-router-dom';
 // import { Navigation, Home, Projects, Footer } from './Container/Exports'
 import './index.css';
+import {Home} from "./Container/Exports"
 
 function App() {
   return (
       <>
+      <Home /> 
         {/* <Routes>
           <Route path="/" element={<Home />} /> */}
           {/* <Route path="/themes" element={<Projects />} />
