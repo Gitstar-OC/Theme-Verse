d8db25aecf86ce2025b478001f0b5483d077b4aa
Gitstar-OC <chandankarom07@gmail.com>
2024-05-18 13:57:22 +0530

    Updated main.jsx

--- Theme-Verse/src/Container/Main.jsx
+++ Theme-Verse/src/Container/Main.jsx
@@ -1,12 +1,31 @@
 // import React from 'react'
 
 
-const Main = () => {
-    return (
-        <section className='h-[100vh] theme-bg' id='section'>
-          <p className='theme-text'>Hello</p>
-        </section>
-    );
-}
+import HomeLogo from "./assets/HomeLogo.svg";
+import ExampleImage from "../assets/example-image.png"; // Ensure this path is correct
 
-export default Main
+const Main = () => {
+  return (
+    <section className='h-[100vh] theme-bg flex items-center justify-center' id='section'>
+      <div className='flex flex-col md:flex-row items-center justify-center w-full max-w-screen-xl mx-auto px-4'>
+        <div className='text-center md:text-left md:w-1/2 mb-8 md:mb-0'>
+          <img src={HomeLogo} alt="Home Logo" className='mx-auto md:mx-0 mb-8' />
+          <h1 className='font-cF text-[64px] font-normal leading-normal text-heading mb-4'>App Heading</h1>
+          <p className='font-cL text-[32px] font-normal leading-normal text-third mb-8'>
+            This is a description about the app. It provides details about the features and functionalities.
+          </p>
+        </div>
+        <div className='relative md:w-1/2'>
+          <img
+            src={ExampleImage}
+            alt="Example"
+            className='rounded-[25px] border-3 border-third'
+          />
+          <div className='absolute inset-0 rounded-[25px] bg-third opacity-50 md:opacity-0' />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Main;
