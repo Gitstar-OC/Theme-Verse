287f196d33a690f997ce0524459e39971ec34782
Gitstar-OC <chandankarom07@gmail.com>
2024-05-21 12:19:32 +0530

    Changed the filters property

--- Theme-Verse/src/components/FilterSidebar.jsx
+++ Theme-Verse/src/components/FilterSidebar.jsx
@@ -9,38 +9,38 @@ const FilterSidebar = () => {
       <div className="space-y-4 w-full">
         <div className="flex items-center group">
           <input type="checkbox" id="payment-app" className="mr-2 custom-checkbox" />
-          <label htmlFor="payment-app" className="filter-text group-hover:text-[#09FFB5]">Payment App</label>
+          <label htmlFor="payment-app" className="filter-text">Payment App</label>
         </div>
         <div className="flex items-center group">
           <input type="checkbox" id="ai-website" className="mr-2 custom-checkbox" />
-          <label htmlFor="ai-website" className="filter-text group-hover:text-[#09FFB5]">AI Website</label>
+          <label htmlFor="ai-website" className="filter-text">AI Website</label>
         </div>
         <div className="flex items-center group">
           <input type="checkbox" id="shoe-website" className="mr-2 custom-checkbox" />
-          <label htmlFor="shoe-website" className="filter-text group-hover:text-[#09FFB5]">Shoe Website</label>
+          <label htmlFor="shoe-website" className="filter-text">Shoe Website</label>
         </div>
         <div className="flex items-center group">
           <input type="checkbox" id="restaurant" className="mr-2 custom-checkbox" />
-          <label htmlFor="restaurant" className="filter-text group-hover:text-[#09FFB5]">Restaurant</label>
+          <label htmlFor="restaurant" className="filter-text">Restaurant</label>
         </div>
       </div>
       <div className="border-b-3 border-heading w-full my-4"></div>
       <div className="space-y-4 w-full">
         <div className="flex items-center group">
           <input type="checkbox" id="chakra-ui" className="mr-2 custom-checkbox" />
-          <label htmlFor="chakra-ui" className="filter-text group-hover:text-[#09FFB5]">Chakra UI</label>
+          <label htmlFor="chakra-ui" className="filter-text">Chakra UI</label>
         </div>
         <div className="flex items-center group">
           <input type="checkbox" id="react" className="mr-2 custom-checkbox" />
-          <label htmlFor="react" className="filter-text group-hover:text-[#09FFB5]">React</label>
+          <label htmlFor="react" className="filter-text">React</label>
         </div>
         <div className="flex items-center group">
           <input type="checkbox" id="tailwind" className="mr-2 custom-checkbox" />
-          <label htmlFor="tailwind" className="filter-text group-hover:text-[#09FFB5]">Tailwind</label>
+          <label htmlFor="tailwind" className="filter-text">Tailwind</label>
         </div>
         <div className="flex items-center group">
           <input type="checkbox" id="vite" className="mr-2 custom-checkbox" />
-          <label htmlFor="vite" className="filter-text group-hover:text-[#09FFB5]">Vite</label>
+          <label htmlFor="vite" className="filter-text">Vite</label>
         </div>
       </div>
       <div className="border-b-3 border-heading w-full my-4"></div>
