69e4013fe88da7092bf0d17033fc6266a550aa9d
Gitstar-OC <chandankarom07@gmail.com>
2024-05-21 11:07:20 +0530

    Added side bar in themes section

--- Theme-Verse/src/Container/Themes.jsx
+++ Theme-Verse/src/Container/Themes.jsx
@@ -1,13 +1,21 @@
-// import React from 'react'
-import { Navigation, Footer } from '../components/Exports'
+// Themes.js
+import React from 'react';
+import { Navigation, Footer } from '../components/Exports';
+import FilterSidebar from './FilterSidebar';
 
 const Themes = () => {
   return (
     <>
       <Navigation />
+      <div className="flex">
+        <FilterSidebar />
+        <div className="main-content flex-1 p-4">
+          {/* Your main content goes here */}
+        </div>
+      </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default Themes
+export default Themes;
