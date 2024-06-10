fbf85b790d45bee139a25096cd8dec7d3bf6b09d
OC <chandankarom07@gmail.com>
2024-06-10 16:41:19 +0530

    Added add to cart, and see preview option along with hover effects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import { FaArrowCircleRight } from "react-icons/fa";
 import { projects } from "../Constants/index";
 import { useNavigate } from "react-router-dom";
+import { FaOpencart, FaRegEye } from 'react-icons/fa';
 
 const Projects = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -52,50 +53,58 @@ const Projects = () => {
     return (
       <div
         key={project.name}
-        className={`projectCard flex ${isEven ? 'flex-row-reverse' : ''} items-center justify-between  my-6 p-4 rounded-2xl  m-0 max-w-[1000px] border-solid border-dark-bg border-2 dark:border-white  `}
+        className={` flex ${isEven ? 'flex-row-reverse' : ''}  projectCard items-start justify-start my-6 p-4`}
+        style={{ marginLeft: '8rem', marginBottom: '2rem', marginRight: "8rem" }} // Adjust left, right, and bottom margins
       >
-        <div className="projectIframeContainer">
+        <div className='projectIframeContainer'>
           <iframe
             src={project.url}
             title={project.name}
-            className="projectIframe "
+            className='projectIframe border-black border-2 dark:border-white'
           />
         </div>
-        <div className="projectDetails">
-          <h3 className="text-xl font-bold font-cF dark:text-white">
-            {project.name}
-          </h3>
-          <p className="text-md font-cL dark:text-slate-400">
-            {project.description}
-          </p>
+        <div className='ml-20 mt-4 max-w-160 mr-20'> {/* projectDetails Add left margin for the project details */}
+          <div className='mt-4  mr-8 border-black border-solid border-2 dark:border-white'> </div>
+          <h3 className='mt-10 text-[2.5rem] font-cF dark:text-white'>{project.name}</h3>
+          <p className='mt-4 text-[1.5rem] font-cL dark:text-slate-400 mb-20'>{project.description}</p>
+
+          <div className=' mr-8 border-black border-solid border-2 dark:border-white '></div> 
+          <div className=" mt-20 mb-10 flex items-center space-x-1 font-cL justify-center text-[2.5rem] dark:text-white cursor-pointer">
+            <div className="projectItem">
+            <FaRegEye className='mb-3'/> 
+            <span className=""> See Preview</span>
+            </div>
+          </div>
+          <div className="  mt-10 mb-20 flex items-center space-x-1 font-cL text-[2.5rem] justify-center cursor-pointer dark:text-white" onClick="">
+              <div className="projectItem">
+              <FaOpencart className=' mb-3'/>
+              <span className="">Add to Cart</span>
+              </div>
+            </div>
+        <div className='mr-8 mb-4 justify-end border-black border-solid border-2 dark:border-white'></div> 
         </div>
       </div>
     );
   };
 
   return (
-    <div
-      className=" theme-bg flex flex-col items-center justify-start  mb-20 gap-12"
-      id="section"
-    >
+    <div className='theme-bg flex flex-col items-center justify-center mb-20 mt-20 gap-12' id='section'>
       <div
         ref={sectionRef}
         className={`rectangleDiv transition-all duration-1000 transform ${
           isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
-        } mb-8`}
+        } mb-8 flex items-center justify-center`}
       >
-        <div className="projectItem text-[48px] ">Themes</div>
+        <div className="projectItem text-[3rem] text-center">Themes</div>
       </div>
-      <div className="space-y-10 mb-4">
-        {projects
-          .slice(0, 3)
-          .map((project, index) => renderProject(project, index))}
+      <div className='flex flex-col items-start space-y-10 px-4 w-full'> {/* Align projects to the start with padding */}
+        {projects.slice(0, 3).map((project, index) => renderProject(project, index))}
       </div>
       <div
         ref={seeAllRef}
-        className={`smallDiv mt-10 transition-all duration-1000 transform ${
+        className={`smallDiv transition-all duration-1000 transform ${
           isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
-        } `}
+        } flex items-center justify-center`}
       >
         <div
           className={`projectItem text-[40px] cursor-pointer ${
