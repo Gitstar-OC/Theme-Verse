8b163f78ab6eaafed73cfe79c9740c875ab60433
OC <chandankarom07@gmail.com>
2024-06-12 11:09:27 +0530

    Added basic design of the form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,8 @@
 import { useState, useRef, useEffect } from "react";
+import { Profile } from "./Exports";
 import { IoMdPerson, IoMdMail } from "react-icons/io";
 import { FaGithub, FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 import { BsFillSendFill } from "react-icons/bs";
 // import "../index.css"; // Ensure you import the CSS file
 
@@ -53,46 +55,90 @@ const ContactForm = () => {
           isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
         } mb-8 mt-8 flex items-center justify-center`}
       >
-        <div className="projectItem text-[3rem] text-center font-cF text-white dark:text-black">Contact</div>
+        <div className="projectItem text-[3rem] text-center font-cF">Contact</div>
       </div>
 
-      <div className="shadow-lg rounded-l-[25px] bg-gray-800 bg-opacity-20 relative p-10 w-[711px] flex flex-col space-y-8">
-        <div className="text-[3rem] font-cF text-white dark:text-black">
-          Get In Touch
-        </div>
-        <div className="flex space-x-6">
-          <div className="bg-gray-300 rounded-[10px] p-4 flex-1">
-            <span className="font-['Inter'] text-[16px] text-black">Name</span>
+      <div className="flex space-x-10">
+        <div className="shadow-lg rounded-l-[25px] bg-gray-800 bg-opacity-20 relative p-10 w-[711px] flex flex-col space-y-8 contactDiv">
+          <div className="text-[3rem] font-cF text-black dark:text-white">
+            Get In Touch
           </div>
-          <div className="bg-gray-300 rounded-[10px] p-4 flex-1">
-            <span className="font-['Inter'] text-[16px] text-black">Email</span>
+          <div className="flex space-x-6">
+            <div className="bg-gray-300 rounded-[10px] p-4 flex-1">
+              <span className="font-['Inter'] text-[16px] text-black">Name</span>
+            </div>
+            <div className="bg-gray-300 rounded-[10px] p-4 flex-1">
+              <span className="font-['Inter'] text-[16px] text-black">Email</span>
+            </div>
+          </div>
+          <div className="flex space-x-6">
+            <div className="bg-gray-300 rounded-[10px] p-4 flex-1">
+              <span className="font-['Inter'] text-[16px] text-black">Position</span>
+            </div>
+            <div className="bg-gray-300 rounded-[10px] p-4 flex-1">
+              <select className="font-['Inter'] text-[16px] text-black bg-transparent border-none outline-none w-full">
+                <option value="instagram">Instagram</option>
+                <option value="linkedin">Linkedin</option>
+                <option value="facebook">Facebook</option>
+                <option value="github">Github</option>
+              </select>
+            </div>
+          </div>
+          <div className="bg-gray-300 rounded-[10px] p-4 w-full">
+            <span className="font-['Inter'] text-[16px] text-black">Company</span>
+          </div>
+          <div className="bg-gray-300 rounded-[10px] p-4 w-full">
+            <span className="font-['Inter'] text-[16px] text-black">Link</span>
+          </div>
+          <div className="bg-gray-300 rounded-[15px] p-4 w-full h-[150px]">
+            <span className="font-['Inter'] text-[16px] text-black">Message</span>
+          </div>
+          <div className="flex items-center justify-center rounded-[15px] p-4 border-2 cursor-pointer border-[#0f94b2] bg-[#a6d2ea] dark:border-[#a6d2ea] dark:bg-[#000000]" id="send">
+            <div className="projectItem">
+              <BsFillSendFill className="mr-2 mb-4 text-[2rem]" />
+              <span className="projectItem text-[2.5rem] text-center font-cL">Send</span>
+            </div>
           </div>
         </div>
-        <div className="flex space-x-6">
-          <div className="bg-gray-300 rounded-[10px] p-4 flex-1">
-            <span className="font-['Inter'] text-[16px] text-black">Position</span>
+
+        <div className="shadow-[0px_10px_15px_-3px_#1A3DF8] rounded-r-[25px] bg-[rgba(74,74,74,0.2)] relative flex flex-col p-[31px_0_19.8px_13px] w-[361px]">
+          <div className="self-center w-[200px] h-[196.5px]">
+            <Profile size="small" />
           </div>
-          <div className="bg-gray-300 rounded-[10px] p-4 flex-1">
-            <select className="font-['Inter'] text-[16px] text-black bg-transparent border-none outline-none w-full">
-              <option value="instagram">Instagram</option>
-              <option value="linkedin">Linkedin</option>
-              <option value="facebook">Facebook</option>
-              <option value="github">Github</option>
-            </select>
+          <div className="bg-[#FFFFFF] my-8 w-full h-[1px]"></div>
+          <div className="flex flex-col">
+            <div className="mb-4 font-['Akaya_Telivigala'] text-[28px] text-[#FFFFFF] dark:text-black">
+              Mail Me
+            </div>
+            <div className="flex items-center text-[24px] font-['Chilanka'] text-[#FFFFFF] dark:text-black">
+              <IoMdMail className="mr-2" />
+              chandankarom07@gmail.com
+            </div>
           </div>
-        </div>
-        <div className="bg-gray-300 rounded-[10px] p-4 w-full">
-          <span className="font-['Inter'] text-[16px] text-black">Company</span>
-        </div>
-        <div className="bg-gray-300 rounded-[10px] p-4 w-full">
-          <span className="font-['Inter'] text-[16px] text-black">Link</span>
-        </div>
-        <div className="bg-gray-300 rounded-[15px] p-4 w-full h-[150px]">
-          <span className="font-['Inter'] text-[16px] text-black">Message</span>
-        </div>
-        <div className="flex items-center justify-center bg-black rounded-[15px] p-4 border-2 border-[#0F94B2] text-[#0F94B2] cursor-pointer">
-          <BsFillSendFill className="mr-2" />
-          <span className="font-cL text-[2.5rem]">Send</span>
+          <div className="bg-[#FFFFFF] my-8 w-full h-[1px]"></div>
+          <div className="flex flex-col">
+            <div className="mb-4 font-['Akaya_Telivigala'] text-[28px] text-[#FFFFFF] dark:text-black">
+              Follow On Github
+            </div>
+            <a
+              href="https://github.com/Gitstar-OC"
+              className="flex items-center text-[24px] font-['Chilanka'] text-[#FFFFFF] dark:text-black cursor-pointer"
+            >
+              <FaGithub className="mr-2" />
+              Gitstar-OC
+            </a>
+          </div>
+          <div className="bg-[#FFFFFF] my-8 w-full h-[1px]"></div>
+          <div className="flex flex-col">
+            <div className="mb-4 font-['Akaya_Telivigala'] text-[28px] text-[#FFFFFF] dark:text-black">
+              Reach me on Social Media
+            </div>
+            <div className="text-[24px] font-['Chilanka'] text-[#FFFFFF] dark:text-black">
+              <FaInstagram className="inline mr-2" /> Instagram<br />
+              <FaLinkedin className="inline mr-2" /> LinkedIn<br />
+              <FaSquareXTwitter className="inline mr-2" /> Twitter
+            </div>
+          </div>
         </div>
       </div>
     </div>
