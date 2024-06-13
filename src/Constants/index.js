647855fa3caa41226d31d1ef81b48f21c2f7a380
OC <chandankarom07@gmail.com>
2024-06-13 14:35:23 +0530

    Added price of each thing in the constant's file

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -32,56 +32,64 @@ export const projects = [
     name: 'Payment Website',
     url: 'https://gitstar-oc.github.io/Payment-website/',
     description: 'A comprehensive payment website with secure transaction features and a user-friendly interface.',
-    technologies: ['React', 'Tailwind', 'Vite']
+    technologies: ['React', 'Tailwind', 'Vite'],
+    price: 50
   },
   {
     id: "1",
     name: 'Nike Website',
     url: 'https://gitstar-oc.github.io/Nike/',
     description: 'An e-commerce site for Nike, showcasing the latest products with an intuitive shopping experience.',
-    technologies: ['React', 'Tailwind', 'Vite']
+    technologies: ['React', 'Tailwind', 'Vite'],
+    price: 100
   },
   {
     id: "2",
     name: 'Brainware AI',
     url: 'https://gitstar-oc.github.io/BrainwareAI/',
     description: 'An AI-based platform that provides intelligent solutions for various industries.',
-    technologies: ['React', 'Tailwind', 'Vite']
+    technologies: ['React', 'Tailwind', 'Vite'],
+    price: 200
   },
   {
     id: "3",
     name: 'Little Lemon',
     url: 'https://gitstar-oc.github.io/Little-Lemon/',
     description: 'A charming website for a small restaurant, emphasizing a cozy and delightful dining experience.',
-    technologies: ['React', 'Chakra UI', 'CSS']
+    technologies: ['React', 'Chakra UI', 'CSS'],
+    price: 100
   },
   {
     id: "4",
     name: 'GPT',
     url: 'https://gitstar-oc.github.io/GPT3/',
     description: 'A showcase of the capabilities of GPT-3, demonstrating advanced AI text generation and interaction.',
-    technologies: ['React', 'CSS']
+    technologies: ['React', 'CSS'],
+    price: 150
   },
   {
     id: "5",
     name: 'GERICHT',
     url: 'https://gitstar-oc.github.io/GERICHT/',
     description: 'A modern restaurant website with an elegant design and a focus on fine dining.',
-    technologies: ['React', 'Tailwind']
+    technologies: ['React', 'Tailwind'],
+    price: 150
   },
   {
     id: "6",
     name: 'Grilli',
     url: 'https://gitstar-oc.github.io/Grilli/',
     description: 'A grilling-themed website featuring recipes, tips, and a community for barbecue enthusiasts.',
-    technologies: ['Vanilla JavaScript', 'CSS']
+    technologies: ['Vanilla JavaScript', 'CSS'],
+    price: 200
   },
   {
     id: "7",
     name: 'Sushiman',
     url: 'https://gitstar-oc.github.io/Sushi_Resto/',
     description: 'A sophisticated sushi restaurant website with beautiful imagery and an easy-to-navigate menu.',
-    technologies: ['Vanilla JavaScript', 'CSS']
+    technologies: ['Vanilla JavaScript', 'CSS'],
+    price: 80
   }
 ];
 
