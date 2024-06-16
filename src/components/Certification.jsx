99871cb9d9de06339be7d62a3311468cb51ad506
OC <chandankarom07@gmail.com>
2024-06-16 20:29:51 +0530

    Completed the info section, resume link is remain to be added other things are done

--- src/components/Certification.jsx
+++ src/components/Certification.jsx
@@ -4,7 +4,11 @@ import { useEffect, useRef, useState } from "react"
 
 const Certification = () => {
   const sectionRef = useRef(null);
+  const metaRef = useRef(null);
+  const stanfordRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
+  const [metaVisible, setMetaVisible] = useState(false);
+  const [stanfordVisible, setStanfordVisible] = useState(false);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -29,23 +33,106 @@ const Certification = () => {
       }
     };
   }, []);
+
+  useEffect(() => {
+    const handleScrollObserver = (entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          if (entry.target === metaRef.current) {
+            setMetaVisible(true);
+          } else if (entry.target === stanfordRef.current) {
+            setStanfordVisible(true);
+          }
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleScrollObserver, { threshold: 0.5 });
+
+    if (metaRef.current) {
+      observer.observe(metaRef.current);
+    }
+
+    if (stanfordRef.current) {
+      observer.observe(stanfordRef.current);
+    }
+
+    return () => {
+      if (metaRef.current) {
+        observer.unobserve(metaRef.current);
+      }
+
+      if (stanfordRef.current) {
+        observer.unobserve(stanfordRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <>
-    <div className="border-t-2 my-12 lg:my-24 border-heading  flex flex-col items-center">
-    <div
+    <div className="border-t-2 my-12 border-heading flex flex-col items-center mx-12 lg:mx-32 ">
+      <div
         ref={sectionRef}
         className={`cartDiv transition-all duration-1000 transform ${
           isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
-        } mb-8 mt-8 flex items-center justify-center`}
+        } mb-8 mt-10 flex items-center justify-center `}
       >
         <div className="projectItem text-[2.5rem] text-center font-cF">
           Certification
         </div>
       </div>
-      <img src={Meta} alt="meta certificate" className="rounded-[25px] w-[45rem] h-[30rem] my-20" />
-      <img src={Stanford} alt="Stanford Certificate" className="rounded-[25px] w-[45rem] h-[30rem]"/>
+
+      <div className="flex flex-col lg:flex-row items-center lg:items-start mb-20 w-full mt-10">
+        <div 
+          ref={metaRef} 
+          className={`flex flex-col lg:flex-row items-center lg:items-start w-full lg:mr-20 ml-20 transition-transform duration-1000 ${
+            metaVisible ? "content-slide-in" : "translate-y-10 opacity-0"
+          }`}
+        >
+          <img 
+            src={Meta} 
+            alt="meta certificate" 
+            className="rounded-[25px] w-[45rem] h-[30rem] transform transition-transform duration-2000 hover:scale-105 border-2 dark:border-white border-black mb-8 lg:mb-0 lg:mr-8" 
+          />
+          <div className="flex flex-col items-start justify-between h-[30rem] w-full lg:w-auto">
+            <div className="text-[2.5rem] font-cF dark:text-white  mt-4">
+              Meta Certification
+            </div>
+            <div className="text-[2rem] font-cL text-black dark:text-white flex-grow mt-8">
+              This certificate is offered after completing the 9 course series and submitting all the project that are given.
+            </div>
+            <a href="https://www.coursera.org/account/accomplishments/professional-cert/CP2NHQXCUXGL" className="mb-4 underline text-[2rem] font-cL mt-auto dark:text-white hover:text-secondary dark:hover:text-secondary">
+              View Certificate
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row-reverse items-center lg:items-start mb-20 w-full ">
+        <div 
+          ref={stanfordRef} 
+          className={`flex flex-col lg:flex-row-reverse items-center lg:items-start w-full ml-20 mr-20 transition-transform duration-1000 ${
+            stanfordVisible ? "image-slide-in" : "translate-y-10 opacity-0"
+          }`}
+        >
+          <img 
+            src={Stanford} 
+            alt="Stanford Certificate" 
+            className="rounded-[25px] w-[45rem] h-[30rem] transform transition-transform duration-2000 hover:scale-105 border-2 dark:border-white border-black mb-8 lg:mb-0 lg:ml-8" 
+          />
+          <div className="flex flex-col items-start justify-between h-[30rem] w-full lg:w-auto">
+            <div className="text-[2.5rem] font-cF dark:text-white mt-4">
+              Stanford Certification
+            </div>
+            <div className="text-[2rem] font-cL text-black dark:text-white flex-grow mt-10">
+              This certificate is offered after completing the first course of Machine Learning Specialization by Stanford (Learning is in Progress).
+            </div>
+            <a href="https://www.coursera.org/account/accomplishments/verify/3WJMWE6D7N7B" className="underline text-[2rem] font-cL mt-auto dark:text-white hover:text-secondary dark:hover:text-secondary mb-4">
+              View Certificate
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
-    </>
   )
 }
 
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,9 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Profile } from "./Exports";
 import { Link } from "react-router-dom";
+import { IoMdMail } from "react-icons/io";
+import { FaFileDownload } from "react-icons/fa";
 import "../index.css"; // Ensure you import the CSS file
 
 const Info = () => {
+  const sectionRef = useRef(null);
+  const seeAllRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+            observer.disconnect();
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+    if (seeAllRef.current) {
+      observer.observe(seeAllRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+      if (seeAllRef.current) {
+        observer.unobserve(seeAllRef.current);
+      }
+    };
+  }, []);
+
   useEffect(() => {
     const onMouseMove = (e) => {
       const layers = document.querySelectorAll(".parallax-layer");
@@ -78,12 +114,12 @@ const Info = () => {
     "a",
     "Front",
     "End",
-    "Developer"
-  ]
+    "Developer",
+  ];
 
   const renderWord = (word, index) => {
     const cleanedWord = word.replace(/[.,!]/g, "");
-    if (["Om", "Chandankar", "creator" ,"freelancer", "multiple", "clients" ].includes(cleanedWord)) {
+    if (["Om", "Chandankar", "creator", "freelancer", "multiple", "clients"].includes(cleanedWord)) {
       if (cleanedWord === "Contact" || cleanedWord === "Today") {
         return (
           <Link key={index} to="/contact" className="hover-word special-word">
@@ -105,67 +141,98 @@ const Info = () => {
   };
 
   return (
-    <section
-      className="pt-[55px] h-[calc(90vh-55px)] bg-light-bg dark:bg-dark-bg flex items-center w-full max-w-screen-xl mx-auto px-4 relative mb-0"
-      id="section"
-    >
-      <div className="backgroundAnim backgroundAnim1"></div>
-      <div className="backgroundAnim backgroundAnim2"></div>
-      <div className="backgroundAnim backgroundAnim3"></div>
-      <div className="backgroundAnim backgroundAnim4"></div>
-      <div className="backgroundAnim backgroundAnim5"></div>
-      <div className="parallax-container absolute inset-0 overflow-hidden">
-        <div className="parallax-layer layer1" data-speed="2"></div>
-        <div className="parallax-layer layer2" data-speed="5"></div>
-      </div>
-      <div className="parallax-content flex flex-col sm:flex-row items-center w-full z-10 relative">
-        <div
-          id="content"
-          className="text-center sm:text-left w-full sm:w-1/2 mb-0 sm:mb-0 sm:mr-4 flex flex-col"
-        >
-          <h1 className="font-cL text-[3rem] font-normal leading-normal text-heading mb-4 relative inline-block self-start break-words">
-             <h1 className="inline animate-bounce">👋🏻</h1> Hello, Nice to meet you!
-            <svg
-              className="absolute bottom-[-10px] left-0 w-[100%] h-5"
-              viewBox="0 0 50 20"
-              preserveAspectRatio="none"
-            >
-              <path
-                d="M0 10 C 10 20, 30 0, 50 10"
-                stroke="url(#rainbow)"
-                strokeWidth="5"
-                fill="transparent"
-              />
-              <defs>
-                <linearGradient id="rainbow">
-                  <stop offset="0%" stopColor="#FF0000" />
-                  <stop offset="16.6%" stopColor="#FF7F00" />
-                  <stop offset="33.3%" stopColor="#FFFF00" />
-                  <stop offset="50%" stopColor="#00FF00" />
-                  <stop offset="66.6%" stopColor="#0000FF" />
-                  <stop offset="83.3%" stopColor="#4B0082" />
-                  <stop offset="100%" stopColor="#8B00FF" />
-                </linearGradient>
-              </defs>
-            </svg>
-          </h1>
-          <p
-            id="hover-paragraph"
-            className="font-cL text-[1.75rem] mt-4 font-normal leading-normal mb-8 text-black dark:text-white break-words"
-          >
-            {words.map((word, index) => renderWord(word, index))}
-          </p>
+    <>
+      <section
+        className="pt-[55px] h-[calc(90vh-55px)] bg-light-bg dark:bg-dark-bg flex items-center w-full max-w-screen-xl mx-auto px-4 relative mb-0"
+        id="section"
+      >
+        <div className="backgroundAnim backgroundAnim1"></div>
+        <div className="backgroundAnim backgroundAnim2"></div>
+        <div className="backgroundAnim backgroundAnim3"></div>
+        <div className="backgroundAnim backgroundAnim4"></div>
+        <div className="backgroundAnim backgroundAnim5"></div>
+        <div className="parallax-container absolute inset-0 overflow-hidden">
+          <div className="parallax-layer layer1" data-speed="2"></div>
+          <div className="parallax-layer layer2" data-speed="5"></div>
         </div>
-        <div
-          id="image-container"
-          className="w-full sm:w-1/2 flex justify-center sm:justify-end mt-0 sm:mt-0"
-        >
-            <div className=" mb-20 mr-20">
+        <div className="parallax-content flex flex-col sm:flex-row items-center w-full z-10 relative">
+          <div
+            id="content"
+            className="text-center sm:text-left w-full sm:w-1/2 mb-0 sm:mb-0 sm:mr-4 flex flex-col"
+          >
+            <h1 className="font-cL text-[3rem] font-normal leading-normal text-heading mb-4 relative inline-block self-start break-words">
+              <h1 className="inline animate-bounce">👋🏻</h1> Hello, Nice to meet you!
+              <svg
+                className="absolute bottom-[-10px] left-0 w-[100%] h-5"
+                viewBox="0 0 50 20"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M0 10 C 10 20, 30 0, 50 10"
+                  stroke="url(#rainbow)"
+                  strokeWidth="5"
+                  fill="transparent"
+                />
+                <defs>
+                  <linearGradient id="rainbow">
+                    <stop offset="0%" stopColor="#FF0000" />
+                    <stop offset="16.6%" stopColor="#FF7F00" />
+                    <stop offset="33.3%" stopColor="#FFFF00" />
+                    <stop offset="50%" stopColor="#00FF00" />
+                    <stop offset="66.6%" stopColor="#0000FF" />
+                    <stop offset="83.3%" stopColor="#4B0082" />
+                    <stop offset="100%" stopColor="#8B00FF" />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </h1>
+            <p
+              id="hover-paragraph"
+              className="font-cL text-[1.75rem] mt-4 font-normal leading-normal mb-8 text-black dark:text-white break-words"
+            >
+              {words.map((word, index) => renderWord(word, index))}
+            </p>
+            <div className="flex flex-col sm:flex-row items-center mt-8">
+              <Link to="/contact" className="w-full sm:w-auto mb-4 sm:mb-0 sm:mr-4">
+                <div
+                  ref={seeAllRef}
+                  className={`smallDiv transition-all duration-1000 transform ${
+                    isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+                  } flex items-center justify-center cursor-pointer`}
+                >
+                  <div
+                    className={`projectItem text-[40px] cursor-pointer ${isVisible ? "animate-bounce" : ""}`}
+                  >
+                    <IoMdMail className="mr-4" /> Contact Me
+                  </div>
+                </div>
+              </Link>
+              <a href="#" className="w-full sm:w-auto">
+                <div
+                  className={`smallDiv transition-all duration-1000 transform ${
+                    isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+                  } flex items-center justify-center cursor-pointer ml-10`}
+                >
+                  <div
+                    className={`projectItem text-[40px] cursor-pointer ${isVisible ? "animate-bounce" : ""}`}
+                  >
+                    <FaFileDownload className="mr-4" /> Get Resume
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div
+            id="image-container"
+            className="w-full sm:w-1/2 flex justify-center sm:justify-end mt-0 sm:mt-0"
+          >
+            <div className="mb-20 mr-20">
               <Profile className="" />
             </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
