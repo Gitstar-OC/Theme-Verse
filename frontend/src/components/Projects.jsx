6756c9990469ef9f0580905137b3cd2eb9fda639
OC <chandankarom07@gmail.com>
2024-07-01 18:57:19 +0530

    Completed the hwole design for ipad

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -5,6 +5,60 @@ import { AddToCart, DoubleAdd, ErrorPreview, ViewPreview } from "../Messages/Exp
 import { IoMdMail } from "react-icons/io";
 import { projects } from "../Constants/index";
 
+const styles = `
+@media (max-width: 1080px) {
+  .projectCard {
+    flex-direction: column !important;
+    margin-left: 2rem;
+    margin-right: 2rem;
+  }
+
+  .projectIframeContainer {
+    width: 100vw;
+  }
+
+  .projectIframe {
+    width: 100%;
+    height: 50vh;
+  }
+
+  .projectInfo {
+    margin: 0 !important;
+    text-align: center;
+  }
+
+  .projectActions {
+    display: flex;
+    justify-content: center;
+    margin-top: 1rem;
+  }
+
+  .projectAction {
+    display: flex;
+    align-items: center;
+    margin: 0 0.5rem;
+    font-size: 2.5rem;
+    cursor: pointer;
+  }
+
+  .projectAction .icon {
+    margin-right: 0.5rem;
+  }
+
+  .tablet {
+    font-size: 3rem;
+  }
+
+  .tablet_description {
+    font-size: 2rem;
+  }
+
+  .projectAction:hover {
+    text-decoration: underline;
+  }
+}
+`;
+
 const Projects = ({ addToCart }) => {
   const [isVisible, setIsVisible] = useState(false);
   const [filteredProjects, setFilteredProjects] = useState(projects);
@@ -123,8 +177,8 @@ const Projects = ({ addToCart }) => {
       <div
         key={project.name}
         data-index={index}
-        className={`flex ${isEven ? 'flex-row-reverse' : ''} projectCard items-start justify-start my-6 p-4 content`}
-        style={{ marginLeft: '8rem', marginBottom: '2rem', marginRight: "8rem" }}
+        className={`flex ${isEven ? 'flex-row-reverse' : ''} projectCard items-start justify-start my-6 p-4 content ml-[6rem] mr-[6rem] `}
+        style={{  marginBottom: '8rem',  }}
       >
         <div className='projectIframeContainer'>
           <iframe
@@ -133,63 +187,74 @@ const Projects = ({ addToCart }) => {
             className='projectIframe border-heading border-2 dark:border-heading'
           />
         </div>
-        <div className='ml-8 mt-2 max-w-160 mr-20'>
-          <div className=' mr-2 mt-4 border-heading border-solid border-2 dark:border-heading'></div>
-          <h3 className='mt-8 text-[2rem] font-cF dark:text-white'>{project.name}</h3>
-          <p className='mt-4 text-[1.5rem] font-cL dark:text-slate-300 mb-8'>{project.description}</p>
+        <div className='ml-8 mt-2 max-w-160 mr-20 projectInfo'>
+          {/* <div className='mr-2 mt-4 border-heading border-solid border-2 dark:border-heading'></div> */}
+          <h3 className='mt-4 text-[3rem] font-cF dark:text-white tablet'>
+            {project.name}
+          </h3>
+          <p className='mt-4 text-[2rem] font-cL dark:text-slate-300 mb-4 tablet_description'>
+            {project.description}
+          </p>
           <div className='mr-2 border-heading border-solid border-2 dark:border-heading'></div>
-          <div className="mt-8 mb-2 flex items-center space-x-1 font-cL justify-center text-[2.5rem] dark:text-white cursor-pointer">
-            <div className="projectItem animate-bounce" onClick={() => handleSeePreview(project.id)}>
-              <FaRegEye className='mb-3' />
-              <span className=""> See Preview</span>
-            </div>
+          <div className="projectActions">
+            <span
+              className="projectAction projectItem animate-bounce text-[2.5rem] mx-32 mt-10"
+              onClick={() => handleSeePreview(project.id)}
+            >
+              <FaRegEye className='icon mb-3' />
+              <span>See Preview</span>
+            </span>
+            <span
+              className="projectAction projectItem animate-bounce text-[2.5rem] mx-32 mt-10"
+              onClick={() => handleAddToCart(project.id)}
+            >
+              <FaOpencart className='icon mb-3' />
+              <span>Add to Cart</span>
+            </span>
           </div>
-          <div className="mt-4 mb-10 flex items-center space-x-1 font-cL text-[2.5rem] justify-center cursor-pointer dark:text-white" onClick={() => handleAddToCart(project.id)}>
-            <div className="projectItem animate-bounce">
-              <FaOpencart className=' mb-3' />
-              <span className="">Add to Cart</span>
-            </div>
-          </div>
-          <div className='mr-2 mb-4  justify-end border-heading border-solid border-2 dark:border-heading'></div>
         </div>
       </div>
     );
   };
 
   return (
-    <div className='theme-bg flex flex-col items-center justify-center mb-20 mt-20 gap-12' id='section'>
-      <div
-        ref={sectionRef}
-        className={`rectangleDiv transition-all duration-1000 transform ${
-          isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
-        } mb-8 flex items-center justify-center`}
-      >
-        <div className="projectItem text-[3rem] text-center">Themes</div>
-      </div>
-      <div className='flex flex-col items-start space-y-10 px-4 w-full overflow-x-hidden'>
-        {projects.slice(0, 3).map((project, index) => renderProject(project, index))}
-      </div>
-      <div
-        ref={seeAllRef}
-        className={`smallDiv transition-all duration-1000 transform ${
-          isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
-        } flex items-center justify-center`}
-      >
-        <Link to="/themes"> <div
-          className={`projectItem text-[40px] cursor-pointer ${
-            isVisible ? "animate-bounce" : ""
-          }`}
+    <>
+      <style>{styles}</style>
+      <div className='theme-bg flex flex-col items-center justify-center mb-20 mt-20 gap-12' id='section'>
+        <div
+          ref={sectionRef}
+          className={`rectangleDiv transition-all duration-1000 transform ${
+            isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+          } mb-8 flex items-center justify-center`}
         >
-          See all <FaArrowCircleRight className="ml-4" />
-        </div></Link>
-        
+          <div className="projectItem text-[3rem] text-center">Themes</div>
+        </div>
+        <div className='flex flex-col items-start space-y-10 px-4 w-full overflow-x-hidden'>
+          {projects.slice(0, 3).map((project, index) => renderProject(project, index))}
+        </div>
+        <div
+          ref={seeAllRef}
+          className={`smallDiv transition-all duration-1000 transform ${
+            isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+          } flex items-center justify-center`}
+        >
+          <Link to="/themes">
+            <div
+              className={`projectItem text-[40px] cursor-pointer ${
+                isVisible ? "animate-bounce" : ""
+              }`}
+            >
+              See all <FaArrowCircleRight className="ml-4" />
+            </div>
+          </Link>
+        </div>
+        {showAddToCart && <AddToCart onClose={handleAddToCartClose} />}
+        {showDoubleAdd && <DoubleAdd onClose={handleDoubleAddClose} />}
+        {showErrorPreview && <ErrorPreview onClose={handleErrorPreviewClose} />}
+        {showViewPreview && <ViewPreview projectId={currentProjectId} onClose={handleViewPreviewClose} />}
       </div>
-      {showAddToCart && <AddToCart onClose={handleAddToCartClose} />}
-      {showDoubleAdd && <DoubleAdd onClose={handleDoubleAddClose} />}
-      {showErrorPreview && <ErrorPreview onClose={handleErrorPreviewClose} />}
-      {showViewPreview && <ViewPreview projectId={currentProjectId} onClose={handleViewPreviewClose} />}
-    </div>
+    </>
   );
-}
+};
 
 export default Projects;
